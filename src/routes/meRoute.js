ca6c85e90d8a59d36f8a540fc7a76b86b1e140f7
doananhkiet886 <doananhkiet886@gmail.com>
2023-09-02 13:28:10 +0700

    refactor meRoute

--- src/routes/meRoute.js
+++ src/routes/meRoute.js
@@ -2,6 +2,6 @@ const router = require('express').Router()
 
 const meController = require('~/controllers/meController')
 
-router.get('/store', meController.store)
+router.get('/store/courses', meController.store)
 
 module.exports = router
