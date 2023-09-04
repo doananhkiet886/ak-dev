6f3ee3eda14bb3d6b99383d2352bdac32f5dedd6
doananhkiet886 <doananhkiet886@gmail.com>
2023-09-04 19:20:35 +0700

    [FIX BUG] rename variable in /src/public/js/handleSearchCourse.js

--- src/public/js/me/handleSearchCourse.js
+++ src/public/js/me/handleSearchCourse.js
@@ -2,18 +2,19 @@
   const inputSearchElement = document.querySelector('.dashboard .input-search')
   const btnSearchElement = document.querySelector('.dashboard .btn-search')
   const urlParams = new URLSearchParams(window.location.search)
-  let searchValue = urlParams.get('search')
+  let searchedValue = urlParams.get('search')
 
   // when searched, update the value of inputSearchElement and enable btnSearch
-  if (searchValue) {
-    inputSearchElement.value = searchValue
+  if (searchedValue) {
+    inputSearchElement.value = searchedValue
     btnSearchElement.classList.remove('disabled')
   }
 
-  // disable btnSearch when empty input
+  // disable btnSearch when empty input and searched yet
+  // otherwise enable btnSearch
   inputSearchElement.oninput = function() {
     let isEmptyInput = !this.value
-    if (isEmptyInput) {
+    if (isEmptyInput && !searchedValue) {
       btnSearchElement.classList.add('disabled')
       return
     }
